1cdd0d36fb0f5f34b3c8117da2788811bec92151
Korosenseiiiii <138607833+Korosenseiiiii@users.noreply.github.com>
2023-07-05 16:18:22 +0530

    Update __init__.py

--- SaitamaRobot/modules/sql/__init__.py
+++ SaitamaRobot/modules/sql/__init__.py
@@ -1,15 +1,27 @@
-from SaitamaRobot import DB_URI
 from sqlalchemy import create_engine
+
 from sqlalchemy.ext.declarative import declarative_base
+
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+DB_URL = "postgres://erhftgpv:Q5hddUsSrBz1Q8pQTdsGLf3oe9eKdviu@lallah.db.elephantsql.com/erhftgpv"
+
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
+
+    engine = create_engine(DB_URL, client_encoding="utf8")
+
     BASE.metadata.bind = engine
+
     BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+    return scoped_session(sessionmaker(bind=engine, autoflush=True))
+
+
+
 
 
 BASE = declarative_base()
+
 SESSION = start()
